2fb3efbd6a25f016da387bfbf4b4ab4ef30db476
LeonardoDelgado1905 <LeonardoDelgado1905@users.noreply.github.com>
2022-11-29 05:07:21 +0000

    deploy: dd42f6fcdb062e97143c41bc527e353919b16381

--- sketches/workshop3/ImageProcessing/sketch.js
+++ sketches/workshop3/ImageProcessing/sketch.js
@@ -9,8 +9,8 @@ function preload() {
   video_src.hide(); // by default video shows up in separate dom
   maskShader = readShader('./mask.frag', { varyings: Tree.texcoords2 });
   //img = loadImage('/sketches/shaders/tree.jpeg');
-  img = loadImage('./img/lupin.png');
-  //img = loadImage('/sketches/shaders/bjork.jpeg');
+  //img = loadImage('./img/lupin.png');
+  img = loadImage('./img/cuy.webp');
   //img = loadImage('/sketches/shaders/acacias.jpeg');
   //img = loadImage('/sketches/shaders/honda.jpg');
   //img = loadImage('/sketches/shaders/v.jpg');
@@ -38,10 +38,13 @@ function setup() {
       video_src.pause();
     }
   });
-  video_on.position(10, 30);
-  mask = createCheckbox('ridges', false);
-  mask.position(10, 10);
-  mask.style('color', 'white');
+  video_on.position(10, 50);
+  prewitt = createCheckbox('prewitt', false);
+  prewitt.position(10, 10);
+  prewitt.style('color', 'white');
+  ridges = createCheckbox('ridges', false);
+  ridges.position(10, 30);
+  ridges.style('color', 'white');
   shader(maskShader);
   maskShader.setUniform('texture', img);
   emitTexOffset(maskShader, img, 'texOffset');
@@ -50,10 +53,16 @@ function setup() {
 function draw() {
   background(0);
   // /*
-  if (mask.checked()) {
+  if (prewitt.checked()) {
     //maskShader.setUniform('mask', [1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9]);
-    maskShader.setUniform('mask', [-1, -1, -1, -1, 8, -1, -1, -1, -1]);
+    //maskShader.setUniform('mask', [1/16, 2/16, 1/16, 2/16, 4/16, 2/16, 1/16, 2/16, 1/16]);
+    maskShader.setUniform('mask', [5, 5, 5, -3, 0, -3, -3, -3, -3]);
+    //maskShader.setUniform('mask', [-1, -1, -1, -1, 8, -1, -1, -1, -1]);
     //maskShader.setUniform('mask', [0, -1, 0, -1, 5, -1, 0, -1, 0]);
+  }
+  else if(ridges.checked()){
+    maskShader.setUniform('mask', [-1, -1, -1, -1, 8, -1, -1, -1, -1]);
+    //maskShader.setUniform('mask', [1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9]);
   }
   else {
     maskShader.setUniform('mask', [0, 0, 0, 0, 1, 0, 0, 0, 0]);
